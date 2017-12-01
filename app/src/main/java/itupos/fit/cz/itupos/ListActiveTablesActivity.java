c52e8f9f765817ea83a523335c2e09871d61c3dc
dadoedo <d.stredansky@gmail.com>
2017-12-01 11:55:33 +0100

    Davides

---
+++ app/src/main/java/itupos/fit/cz/itupos/ListActiveTablesActivity.java
@@ -0,0 +1,13 @@
+package itupos.fit.cz.itupos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActiveTablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_active_tables);
+    }
+}
--- app/src/main/java/itupos/fit/cz/itupos/SingleTableActivity.java
+++ app/src/main/java/itupos/fit/cz/itupos/SingleTableActivity.java
@@ -7,7 +7,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -26,12 +28,15 @@ public class SingleTableActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_add:
                     mTextMessage.setText(R.string.title_add);
+                    navigationAddSelectedAction();
                     return true;
                 case R.id.navigation_order:
                     mTextMessage.setText("Vypis aktualnych a priobjednanych veci");
+                    navigationOrderSelectedAction();
                     return true;
                 case R.id.navigation_payout:
                     mTextMessage.setText(R.string.title_payout);
+                    navigationPayOutSelectedAction();
                     return true;
             }
             return false;
@@ -50,13 +55,47 @@ public class SingleTableActivity extends AppCompatActivity {
         currentTable = VariableSingleton.getTables()[VariableSingleton.selectedTableId];
         currentTableImage = VariableSingleton.selectedTableImage;
 
-        LinearLayout listLinear = findViewById(R.id.list_linear);
-        //listLinear.addView();
-
         currentTable.setTaken();
         if(currentTable.isTaken()){
             currentTableImage.setColorFilter(Color.RED);
         }
+    }
+
+    private void navigationAddSelectedAction(){
+        LinearLayout listLinear = findViewById(R.id.list_linear);
+        listLinear.removeAllViews();
+        TextView addedView  = new TextView(this);
+        int addedViewId = View.generateViewId();
+        addedView.setText("hallo hallo Pridavat");
+        //addedView.setTextAlignment();
+        addedView.setId(addedViewId);
+        addedView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        listLinear.addView(addedView);
+    }
+
+    private void navigationPayOutSelectedAction(){
+        LinearLayout listLinear = findViewById(R.id.list_linear);
+        listLinear.removeAllViews();
+        TextView addedView  = new TextView(this);
+        int addedViewId = View.generateViewId();
+        addedView.setText("hallo hallo PayOut");
+        //addedView.setTextAlignment();
+        addedView.setId(addedViewId);
+        addedView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        listLinear.addView(addedView);
+    }
+
+
+    private void navigationOrderSelectedAction(){
+        LinearLayout listLinear = findViewById(R.id.list_linear);
+        listLinear.removeAllViews();
+        TextView addedView  = new TextView(this);
+        int addedViewId = View.generateViewId();
+        addedView.setText("hallo hallo Order");
+        //addedView.setTextAlignment();
+        addedView.setId(addedViewId);
+        addedView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        listLinear.addView(addedView);
     }
 
 }
