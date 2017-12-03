a1a822dc9266d2685ee2bf6aa1aef044600772e9
ssulaj.martin@gmail.com <Meromero123>
2017-12-03 01:17:20 +0100

    xsulaj00

---
+++ app/src/main/java/itupos/fit/cz/itupos/MyAdapter.java
@@ -0,0 +1,56 @@
+package itupos.fit.cz.itupos;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by martin on 3.12.2017.
+ */
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private Table table;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Table table) {
+        this.table = table;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        //...
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(table.);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return table.;
+    }
+}
--- app/src/main/java/itupos/fit/cz/itupos/SingleTableFinishOrderActivity.java
+++ app/src/main/java/itupos/fit/cz/itupos/SingleTableFinishOrderActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class SingleTableFinishOrderActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private TextView mTextMessage;
 
@@ -39,6 +44,15 @@ public class SingleTableFinishOrderActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // recycle viewer
+        mRecyclerView.setHasFixedSize(true); // maybe delete?
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(VariableSingleton.);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
