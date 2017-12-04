9ea89c1478049247045f1bcf2d20264e4edc3ea4
ssulaj.martin@gmail.com <Meromero123>
2017-12-04 20:25:46 +0100

    xsulaj00

--- app/src/main/java/itupos/fit/cz/itupos/LoginActivity.java
+++ app/src/main/java/itupos/fit/cz/itupos/LoginActivity.java
@@ -88,8 +88,12 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
     public void onBackPressed() {
-        this.finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
